2c5981a7fe8d9cfa984316bb78e9c664cbff0617
Chris <117781333+chrism-la@users.noreply.github.com>
2024-04-13 11:58:46 -0700

    connected to database

--- server/server.js
+++ server/server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+// Express
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 app.listen(3001, () => console.log('Server started on port 3001'));
+
+// Database
+mongoose
+    .connect('mongodb://127.0.0.1:27017/cooked')
+    .then(() => console.log('Connected to DB'))
+    .catch(console.error);
