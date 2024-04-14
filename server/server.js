0e95e9ff8a7e3e27277aa06e0d9407d13e405a6d
Chris <117781333+chrism-la@users.noreply.github.com>
2024-04-14 09:37:04 -0700

    installed dotenv package and created .env file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Express
 const app = express();
@@ -8,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(3001, () => console.log('Server started on port 3001'));
+app.listen(process.env.PORT, () => console.log('Server started on port', process.env.PORT));
 
 // Database
 mongoose
