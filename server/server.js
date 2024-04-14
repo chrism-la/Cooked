14f1e04b9f3ed4747a31c09965c84e3bbb160e4f
Chris <117781333+chrism-la@users.noreply.github.com>
2024-04-14 09:48:17 -0700

    set up for recipes route handler file

--- server/server.js
+++ server/server.js
@@ -4,15 +4,25 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Express
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(process.env.PORT, () => console.log('Server started on port', process.env.PORT));
-
 // Database
+
 mongoose
     .connect('mongodb://127.0.0.1:27017/cooked')
     .then(() => console.log('Connected to DB'))
     .catch(console.error);
+
+// Routes
+
+const recipesRoutes = require('./routes/recipes');
+
+app.use('/recipes', recipesRoutes);
+
+// Port
+
+app.listen(process.env.PORT, () => console.log('Server started on port', process.env.PORT));
