a400e2d39a6feae3bd57512311d1b9e19be66537
Chris <117781333+chrism-la@users.noreply.github.com>
2024-04-16 15:15:00 -0700

    more styling on navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,27 +5,43 @@ import '../scss/Navbar.scss';
 
 export default function Navbar() {
     return (
-        <header>
-            <div className="container">
-                <Link className="logo" to="/">
-                    <FontAwesomeIcon icon={faUtensils} />
-                    <h1 className="title">Cook</h1>
-                    <h2>Book</h2>
-                </Link>
-            </div>
-            <div className="container2">
+        <>
+            <div className="blog-banner">
                 <div>
+                    <p>
+                        JOIN OUR BLOG. <a href="/">SIGN UP</a>
+                    </p>
+                </div>
+            </div>
+            <header>
+                <div className="container">
+                    <Link className="title-card" to="/">
+                        <h1 className="title">Cook</h1>
+                        <h2>BOOK</h2>
+                        <FontAwesomeIcon className="utensil-logo" icon={faUtensils} />
+                    </Link>
+                </div>
+                <div className="container2">
+                    <Link className="home" to="/">
+                        HOME
+                    </Link>
+                    <Link className="recipes" to="/">
+                        RECIPES
+                    </Link>
+                    <Link className="news" to="/">
+                        NEWS
+                    </Link>
+                    <Link className="about Us" to="/">
+                        ABOUT US
+                    </Link>
+                </div>
+                <div className="container3">
+                    <FontAwesomeIcon className="user-logo" icon={faUser} />
                     <Link className="login" to="/">
-                        LogIn
+                        LOGIN
                     </Link>
                 </div>
-                <div>
-                    <Link className="signup" to="/">
-                        SignUp
-                    </Link>
-                </div>
-                <FontAwesomeIcon icon={faUser} />
-            </div>
-        </header>
+            </header>
+        </>
     );
 }
