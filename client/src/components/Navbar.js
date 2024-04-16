27db35160c94b3e901dbee204963f9e2946763c9
Chris <117781333+chrism-la@users.noreply.github.com>
2024-04-16 06:16:17 -0700

    installed sass

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUtensils, faUser } from '@fortawesome/free-solid-svg-icons';
+import '../scss/Navbar.scss';
 
 export default function Navbar() {
     return (
         <header>
             <div className="container">
-                <Link to="/">
+                <Link className="logo" to="/">
                     <FontAwesomeIcon icon={faUtensils} />
                     <h1 className="title">CookBook</h1>
                 </Link>
