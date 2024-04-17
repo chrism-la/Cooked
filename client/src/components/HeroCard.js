4e25b33743b6981bceba0ac1d083c5bbf44709a0
Chris <117781333+chrism-la@users.noreply.github.com>
2024-04-17 15:49:07 -0700

    designing home page

---
+++ client/src/components/HeroCard.js
@@ -0,0 +1,13 @@
+import '../scss/HeroCard.scss';
+
+export default function HeroCard() {
+    return (
+        <div className="card-container">
+            <div className="card">
+                <div className="card-title">
+                    <p>Your official Recipe Book</p>
+                </div>
+            </div>
+        </div>
+    );
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,11 @@
 import Navbar from '../components/Navbar';
+import HeroCard from '../components/HeroCard';
 
 export default function Home() {
     return (
         <div>
             <Navbar />
+            <HeroCard />
         </div>
     );
 }
