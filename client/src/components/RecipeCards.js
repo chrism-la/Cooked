9ef546670234009896a1bfcdfd76d2e15e2b9102
Chris <117781333+chrism-la@users.noreply.github.com>
2024-04-17 08:43:57 -0700

    styled Recipe Cards into grid system

--- client/src/components/RecipeCards.js
+++ client/src/components/RecipeCards.js
@@ -2,10 +2,12 @@ import '../scss/RecipeCards.scss';
 
 export default function RecipeCards({ filteredData }) {
     return (
-        <div className="recipe-cards">
+        <div className="recipe-container">
             {filteredData.map((recipe) => (
-                <div className="img-card">
-                    <img src={recipe.image} alt="Recipe Images"></img>
+                <div className="recipe-cards">
+                    <div className="img-card">
+                        <img src={recipe.image} alt="Recipe Images"></img>
+                    </div>
                     <h2>{recipe.name}</h2>
                 </div>
             ))}
