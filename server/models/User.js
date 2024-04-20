383db256e6c2df1e0d01cd1351fea882ddea9a53
Chris <117781333+chrism-la@users.noreply.github.com>
2024-04-19 19:54:31 -0700

    created a new static method on User model called signup(). this method handles encrypting a new users password as well as creating and saving a new document in the database with theusers email and password

--- server/models/User.js
+++ server/models/User.js
@@ -24,6 +24,13 @@ userSchema.statics.signup = async (email, password) => {
     if (exists) {
         throw Error('Email already exists');
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const user = await this.create({ email, password: hash });
+
+    return user;
 };
 
 module.exports = mongoose.model('User', userSchema);
