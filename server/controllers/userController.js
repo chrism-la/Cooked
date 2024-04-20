4126c8990dde8039521135f436e9417a12103638
Chris <117781333+chrism-la@users.noreply.github.com>
2024-04-19 20:23:51 -0700

    updated signup controller to create a new token when user signsup

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -14,7 +14,9 @@ const signupUser = async (req, res) => {
 
     try {
         const user = await User.signup(email, password);
-        res.status(200).json({ email, user });
+
+        const token = createToken(user._id);
+        res.status(200).json({ email, token });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
