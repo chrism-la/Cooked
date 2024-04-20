55df18a7b6497290a94edf75752867cab6b9c08c
Chris <117781333+chrism-la@users.noreply.github.com>
2024-04-19 20:12:30 -0700

    installed Validator package updated signup method with Validation logic to check emails and passwords

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const Schema = mongoose.Schema;
 
@@ -18,7 +19,20 @@ const userSchema = new Schema(
     { timestamps: true }
 );
 
-userSchema.statics.signup = async (email, password) => {
+userSchema.statics.signup = async function (email, password) {
+    // Validation
+    if (!email || !password) {
+        throw Error('All fields must be filled out!');
+    }
+
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid');
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough');
+    }
+
     const exists = await this.findOne({ email });
 
     if (exists) {
