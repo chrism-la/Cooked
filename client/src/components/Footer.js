7c7b31939286898188940774d71a0596af4fbbe5
Chris <117781333+chrism-la@users.noreply.github.com>
2024-04-20 18:50:32 -0700

    updates to Login styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,26 +1,40 @@
 import '../scss/Footer.scss';
 
-
 export default function Footer() {
     return (
         <footer>
-        <div className="footerContainer">
-            <div className="socialIcons">
-                <a href=""><i className="fab fa-facebook"></i></a>
-                <a href=""><i className="fab fa-instagram"></i></a>
-                <a href=""><i className="fab fa-twitter"></i></a>
-                <a href=""><i className="fab fa-google-plus"></i></a>
-                <a href=""><i className="fab fa-youtube"></i></a>
+            <div className="footerContainer">
+                <div className="socialIcons">
+                    <a href="/">
+                        <i className="fab fa-facebook"></i>
+                    </a>
+                    <a href="/">
+                        <i className="fab fa-instagram"></i>
+                    </a>
+                    <a href="/">
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                    <a href="/">
+                        <i className="fab fa-google-plus"></i>
+                    </a>
+                    <a href="/">
+                        <i className="fab fa-youtube"></i>
+                    </a>
+                </div>
+                <div className="footerNav">
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/recipes">Recipes</a>
+                        </li>
+                        <li>
+                            <a href="/">About Us</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div className="footerNav">
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/recipes">Recipes</a></li>
-                    <li><a href="/">About Us</a></li>
-                </ul>
-            </div>
-        </div>
-    </footer>
-);
+        </footer>
+    );
 }
-    
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import '../scss/Login.scss'
+import { useState } from 'react';
+import '../scss/Login.scss';
 
 export default function LoginForm() {
     const [username, setUsername] = useState('');
@@ -7,15 +7,7 @@ export default function LoginForm() {
     const [remember, setRemember] = useState(false);
 
     const handleSubmit = (event) => {
-        event.preventDefault(); // Prevent the default form submission
-
-        // Here you can perform validation, AJAX requests, etc.
-        // For simplicity, let's just log the values
-        console.log("Username: " + username);
-        console.log("Password: " + password);
-        console.log("Remember Me: " + remember);
-
-        // You can add further logic here, such as sending the data to a server using AJAX
+        event.preventDefault();
     };
 
     return (
@@ -24,44 +16,30 @@ export default function LoginForm() {
                 <form onSubmit={handleSubmit}>
                     <h1>Login</h1>
                     <div className="input-box">
-                        <input
-                            type="text"
-                            placeholder="Username"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            required
-                        />
-                        <i className='bx bxs-user'></i>
+                        <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+                        <i className="bx bxs-user"></i>
                     </div>
                     <div className="input-box">
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                        <i className='bx bxs-lock-alt'></i>
+                        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                        <i className="bx bxs-lock-alt"></i>
                     </div>
                     <div className="remember-forgot">
                         <label>
-                            <input
-                                type="checkbox"
-                                checked={remember}
-                                onChange={(e) => setRemember(e.target.checked)}
-                            />
+                            <input type="checkbox" checked={remember} onChange={(e) => setRemember(e.target.checked)} />
                             Remember me
                         </label>
-                        <a href="#">Forgot password?</a>
+                        <a href="/">Forgot password?</a>
                     </div>
-                    <button type="submit" className="btn">Login</button>
+                    <button type="submit" className="btn">
+                        Login
+                    </button>
                     <div className="register-link">
-                        <p>Don't have an account? <a href="#">Register</a></p>
+                        <p>
+                            Don't have an account? <a href="/">Register</a>
+                        </p>
                     </div>
                 </form>
             </div>
         </div>
     );
 }
-
-
